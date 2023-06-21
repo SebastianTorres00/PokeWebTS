2f5f227f76ba83d93c7fdfaf9522d5ffe6130c4a
Sebastian <sebastiantorrescontacto@gmail.com>
2023-06-21 18:29:00 -0300

    feat: se borran y se mejoran hooks

--- src/features/Home/Hooks/index.ts
+++
@@ -1,5 +0,0 @@
-import useFetchRecipes from "./useFetchRecipes";
-import useHomeRecipes from "./useHomeRecipes";
-import useDeteils from "./useDeteils";
-
-export { useFetchRecipes, useHomeRecipes, useDeteils };
